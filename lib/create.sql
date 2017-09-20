154ad140cdc59553773fc889aec0459ad3773a98
Annette Maza <nettymaza@gmail.com>
2017-09-20 20:51:50 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,10 +1,20 @@
 CREATE TABLE projects (
 id INTEGER PRIMARY KEY,
 title TEXT,
-category INTEGER,
-funding_goal TEXT,
-start_date INTEGER,
-end_date INTEGER
+category TEXT,
+funding_goal NUMERIC,
+start_date TEXT,
+end_date TEXT
+)
+;
+
+CREATE TABLE projects (
+id INTEGER PRIMARY KEY,
+title TEXT,
+category TEXT,
+funding_goal NUMERIC,
+start_date TEXT,
+end_date TEXT
 );
 
 CREATE TABLE users (
