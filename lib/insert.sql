496177a2e747b51ec592a67653b2d56bbc400f3e
Annette Maza <nettymaza@gmail.com>
2017-09-20 21:06:56 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES 
-(1, 'Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'), 
-(2, 'My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01'), 
-(3, 'The next Harry Potter', 'books', 1000.00, '2013-05-30', '2015-05-30'), 
-(4, 'Animal shelter needs dog food', 'charity', 400.00, '2013-03-14', '2013-06-30'), 
-(5, 'Voldement needs a body', 'charity', 6000.00, '2013-03-20', '2013-09-20'), 
-(6, 'The next Inna-Gadda-Davida', 'music', 200.00, '2014-12-30', '2015-12-30'), 
-(7, 'Iguana needs tail operation', 'charity', 2000.00, '2013-10-02', '2013-10-30'), 
-(8, 'I have bed bugs!', 'charity', 800.00, '2014-06-30', '2014-06-31'), 
-(9, 'I want to teach English in China', 'charity', 3000.00, '2013-06-30', '2013-09-30'), 
-(10, 'Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
+(1, 'Help the aviary', 'chaity', 300.00, '2014-06-30', '2014-07-30'),
+(2, 'My book on GOT', 'books', 80.00, '2014-01-01', '2014-01-01'),
+(3, 'The next most romantic nobel', 'books', 7000.00, '2014-05-30', '2015-05-30'),
+(4, 'Help me buy a car', 'charity', 800.00, '2014-03-14', '2014-06-30'),
+(5, 'Help the zoo keeper', 'charity', 7000.00, '2014-03-20', '2014-09-20'),
+(6, 'Help me buy a guitar', 'music', 100.00, '2014-12-30', '2015-12-30'),
+(7, 'My dog got ran over by a car', 'charity', 3000.00, '2014-10-02', '2014-10-30'),
+(8, 'I wanna visit 10 countries', 'charity', 900.00, '2014-06-30', '2014-06-31'),
+(9, 'Help the cats of liberty', 'charity', 4000.00, '2014-06-30', '2014-09-30'),
+(10, 'Help save the quails', 'charity', 500.00, '2012-03-20', '2013-06-30');
 
-INSERT INTO users (id, name, age) VALUES (1, 'Finnebar', 17), (2, 'Bear', 6), (3, 'Iguana', 4), (4, 'Alex', 33), 
-(5, 'Amanda', 24), (6, 'Sophie', 24), (7, 'Rosey', 9), (8, 'Victoria', 23), (9, 'Franz', 100), (10, 'Hermione', 30), 
-(11, 'Voldemort', 90), (12, 'Marisa', 24), (13, 'Swizzle', 4), (14, 'Sirius', 36), (15, 'Albus', 113), (16, 'Squid', 5), 
-(17, 'Whale', 6), (18, 'Pacha', 5), (19, 'Ena', 24), (20, 'Katie', 24);
+INSERT INTO users (id, name, age) VALUES (1, 'Maria', 17), (2, 'Eddo', 6), (3, 'Thom', 4), (4, 'Kate', 33),
+(5, 'Laure', 24), (6, 'Rikki', 24), (7, 'Emma', 9), (8, 'Victoria', 23), (9, 'Clark', 100), (10, 'Ryan', 30),
+(11, 'Willo', 90), (12, 'Pedro', 24), (13, 'Tata', 4), (14, 'David', 36), (15, 'Cassy', 113), (16, 'Jemma', 5),
+(17, 'Tyler', 6), (18, 'Sabrina', 5), (19, 'Jaime', 24), (20, 'Katie', 24);
