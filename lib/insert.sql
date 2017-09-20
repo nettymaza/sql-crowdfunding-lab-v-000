049ea69d60d68879ae00edf3cda150a6e8bc0629
Annette Maza <nettymaza@gmail.com>
2017-09-20 21:07:57 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,12 +1,12 @@
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
 (1, 'Help the aviary', 'chaity', 300.00, '2014-06-30', '2014-07-30'),
-(2, 'My book on GOT', 'books', 80.00, '2014-01-01', '2014-01-01'),
+(2, 'My book on GOT', 'books', 80.00, '2014-01-01', '2015-01-01'),
 (3, 'The next most romantic nobel', 'books', 7000.00, '2014-05-30', '2015-05-30'),
 (4, 'Help me buy a car', 'charity', 800.00, '2014-03-14', '2014-06-30'),
 (5, 'Help the zoo keeper', 'charity', 7000.00, '2014-03-20', '2014-09-20'),
 (6, 'Help me buy a guitar', 'music', 100.00, '2014-12-30', '2015-12-30'),
 (7, 'My dog got ran over by a car', 'charity', 3000.00, '2014-10-02', '2014-10-30'),
-(8, 'I wanna visit 10 countries', 'charity', 900.00, '2014-06-30', '2014-06-31'),
+(8, 'I wanna visit 10 countries', 'charity', 900.00, '2014-06-30', '2015-06-31'),
 (9, 'Help the cats of liberty', 'charity', 4000.00, '2014-06-30', '2014-09-30'),
 (10, 'Help save the quails', 'charity', 500.00, '2012-03-20', '2013-06-30');
 
